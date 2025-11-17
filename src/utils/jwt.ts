0b48fce6d33bd40baf356d9c73b513343b964369
contactochiga <contactochiga@gmail.com>
2025-11-17 10:08:58 +0100

    Update jwt.ts

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,4 +1,4 @@
-import jwt from "jsonwebtoken";
+import * as jwt from "jsonwebtoken";
 import { APP_JWT_SECRET } from "../config/env";
 
 export type APayload = {
@@ -7,14 +7,22 @@ export type APayload = {
   email?: string;
 };
 
-export function signToken(payload: APayload, expiresIn = "7d") {
-  return jwt.sign(payload, APP_JWT_SECRET, { expiresIn });
+export function signToken(payload: APayload, expiresIn: string | number = "7d") {
+  if (!APP_JWT_SECRET) {
+    throw new Error("APP_JWT_SECRET is not defined");
+  }
+
+  return jwt.sign(payload, APP_JWT_SECRET as string, {
+    expiresIn,
+  });
 }
 
 export function verifyToken(token: string) {
   try {
-    return jwt.verify(token, APP_JWT_SECRET) as APayload;
-  } catch (err) {
+    if (!APP_JWT_SECRET) return null;
+
+    return jwt.verify(token, APP_JWT_SECRET as string) as APayload;
+  } catch {
     return null;
   }
 }
