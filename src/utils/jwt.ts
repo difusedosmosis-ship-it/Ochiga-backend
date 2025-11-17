139cb9e4ca1bb835c89b1bd7a808532fed37feb6
contactochiga <contactochiga@gmail.com>
2025-11-17 10:22:29 +0100

    Update jwt.ts

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -2,13 +2,16 @@ import jwt, { SignOptions } from "jsonwebtoken";
 import { APP_JWT_SECRET } from "../config/env";
 
 export type APayload = {
-  sub: string; // user id
+  sub: string;
   role: string;
   email?: string;
 };
 
 export function signToken(payload: APayload, expiresIn: string = "7d") {
-  const options: SignOptions = { expiresIn };
+  const options: SignOptions = {
+    expiresIn: expiresIn as any, // <-- FIX
+  };
+
   return jwt.sign(payload, APP_JWT_SECRET as string, options);
 }
 
