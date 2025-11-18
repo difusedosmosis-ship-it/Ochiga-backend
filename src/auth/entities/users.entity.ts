13f87c966b0af91e02443a787f50cde3e5f2b9f3
contactochiga <contactochiga@gmail.com>
2025-11-18 19:47:12 +0100

    Create users.entity.ts

---
+++ src/auth/entities/users.entity.ts
@@ -0,0 +1,27 @@
+@Entity("users")
+export class Users {
+
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ unique: true, nullable: true })
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column({ default: false })
+  isResident: boolean;
+
+  @Column({ default: false })
+  isEstateOwner: boolean;
+
+  @Column({ nullable: true })
+  homeId: string;
+
+  @Column({ nullable: true })
+  estateId: string;
+}
