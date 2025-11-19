e8ca0f99caa7f29e5a5dacc52c0dab63c0f5200e
difusedosmosis-ship-it <difusedosmosis@gmail.com>
2025-11-19 21:42:57 +0100

    Update estate.module.ts

--- src/estate/estate.module.ts
+++ src/estate/estate.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { EstateController } from './estate.controller';
 import { EstateService } from './estate.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../users/user.entity';
+import { Home } from '../homes/home.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User, Home])],
   controllers: [EstateController],
   providers: [EstateService],
 })
