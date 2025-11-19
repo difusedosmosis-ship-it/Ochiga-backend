553c750d1e06a76360a55ffba96da4a2fa6a9c2c
difusedosmosis-ship-it <difusedosmosis@gmail.com>
2025-11-19 21:38:56 +0100

    Update estate.controller.ts

--- src/estate/estate.controller.ts
+++ src/estate/estate.controller.ts
@@ -1,4 +1,12 @@
-@Post("create-home")
-async createHome(@Body() dto: CreateHomeDto) {
-  return this.estateService.createHome(dto);
+import { Controller, Post, Body } from '@nestjs/common';
+import { EstateService } from './estate.service';
+
+@Controller('estate')
+export class EstateController {
+  constructor(private readonly estateService: EstateService) {}
+
+  @Post('create-home')
+  async createHome(@Body() dto: any) {
+    return this.estateService.createHome(dto);
+  }
 }
